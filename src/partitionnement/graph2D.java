1651c28c43e3f570af1d2809145eb747453b2cd0
pierre <pierre.garcia@u-psud.fr>
2020-01-05 20:34:50 +0100

    error2

--- src/partitionnement/graph2D.java
+++ src/partitionnement/graph2D.java
@@ -9,7 +9,7 @@ public class graph2D {
     double dataY[];
     String dataPath = "generatedFiles/data/";
     String gnuPath = "generatedFiles/gnuplot/";
-    String graphPath = "../graphs";
+    String graphPath = "../graphs/";
 
     public graph2D(String dataName, double dataX[], double dataY[]){
         this.dataName = dataName;
@@ -20,7 +20,7 @@ public class graph2D {
         FileWriter gnuplot = new FileWriter(gnuPath+dataName+".gnu");
         gnuplot.write("set terminal svg size 920,920 \n" +
                 "set output '"+graphPath+dataName+".svg'\n");
-        gnuplot.write("plot \""+ dataPath + dataName + ".d\"");
+        gnuplot.write("plot \"../data" + dataName + ".d\"");
         gnuplot.close();
         FileWriter data = new FileWriter(dataPath + dataName + ".d");
         for (int i =0; i < dataX.length; i += 1){
