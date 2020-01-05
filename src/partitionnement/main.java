3176612e67ad2a8534b3a5f3b59891770b3d20d7
pierre <pierre.garcia@u-psud.fr>
2020-01-05 19:28:28 +0100

    question4

--- src/partitionnement/main.java
+++ src/partitionnement/main.java
@@ -1,6 +1,6 @@
 package partitionnement;
 
-import java.io.IOException;
+import java.io.*;
 
 public class main {
 
@@ -33,9 +33,59 @@ public class main {
         System.out.println(mix.score());
         mg.createAllFiles();
     }
+    public static double[][] dotDExtractor(String fileName) throws FileNotFoundException {
+        try {
+            double data[][] = new double[4000][2];
+            BufferedReader in = new BufferedReader(new FileReader(fileName));
+            String str;
+            int i = 0;
+            while ((str = in.readLine()) != null) {
+                String [] strs = str.split(" ");
+                for (int j = 0; j < strs.length; j += 1){
+                    data[i / 2][i % 2] = Double.parseDouble(strs[j]);
+                    i+=1;
+                }
+            }
+            in.close();
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static double moyenneScore(int nombreCentres, double [][] data){
+        double sum = 0;
+        for (int i =0; i < 20; i += 1){
+            CenterCreator center = new CenterCreator(nombreCentres, 2);
+            double[][] centers = new double[10][2];
+            centers =  center.generateSpacedCenters(data);
+            Mixture mix = new Mixture(data, centers);
+            mix.epoque(50);
+            sum += mix.score();
+        }
+        return sum/10;
+    }
+
+    public static void question4() throws IOException {
+        double [][] data = dotDExtractor("generatedFiles/data/gmm_data.d");
+        for (int i = 0; i < data.length; i +=1){
+            for (int k = 0; k < data[i].length; k += 1){
+                System.out.print(""+data[i][k] + " ");
+            }
+            System.out.println("");
+        }
+        double[] scores = new double[10];
+        FileWriter fw = new FileWriter("generatedFiles/data/deuxDims.d");
+        for (int k = 1; k < 11; k +=1){
+            fw.write(""+moyenneScore(k,data));
+        }
+
+    }
 
     public static void main(String[] arg) throws IOException {
-        question3();
+        question4();
     }
 
 
