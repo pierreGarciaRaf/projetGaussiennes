7b9d1b49ab7822813bba4ff6c0814afdbed1eb23
pierre <pierre.garcia@u-psud.fr>
2020-01-05 17:35:46 +0100

    question1

--- src/partitionnement/main.java
+++ src/partitionnement/main.java
@@ -3,29 +3,24 @@ package partitionnement;
 import java.io.IOException;
 
 public class main {
-    public static void main(String[] arg) throws IOException {
+
+    public static void question1() throws IOException {
         ImageToExtract ext = new ImageToExtract("mms");
         double[][] point = ext.extractAllPixelsColorNormalized();
         CenterCreator center = new CenterCreator(10, 3);
-        double[][] centers = new double[10][3];
-        float[] minlim = new float[3];
-        float[] maxlim = new float[3];
-        for (int i = 0;  i < 3; i++){
-            minlim[i] = 0.0f;
-            maxlim[i] = 1.0f;
+        for (int i =0; i < 10; i += 1){
+            double[][] centers = new double[10][3];
+            centers =  center.generateSpacedCenters(point);
+            Mixture mix = new Mixture(point, centers);
+            mix.epoque(50);
+            MixtureGrapher mg = new MixtureGrapher(mix,"mms","first");
+            System.out.println("at "+i + " score = " + mix.score());
+            mg.createAllFiles();
         }
-        centers =  center.generateSpacedCenters(point);
-        Mixture mix = new Mixture(point, centers);
-        mix.epoque(4);
-        MixtureGrapher mg = new MixtureGrapher(mix,"mms","first");
-        mg.createAllFiles();
-        for(int i = 0; i < mix.getMeans().length; i++){
-            System.out.println("");
-            System.out.print("le centre " + i + " a pour coordonée: ");
-            for(int j = 0; j < mix.getMeans()[0].length; j++){
-                System.out.print(mix.getMeans()[i][j] + ", ");
-            }
-        }
+    }
+
+    public static void main(String[] arg) throws IOException {
+        question1();
     }
 
 
